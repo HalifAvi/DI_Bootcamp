f64cc2a2f34147cc8c3037c87e208d31fa43a09d
HalifAvi <96798674+HalifAvi@users.noreply.github.com>
2022-01-22 14:29:48 +0200

    gold

--- Week4/Day5/XP GOLD/script.js
+++ Week4/Day5/XP GOLD/script.js
@@ -1,167 +1,199 @@
 
-// Return a book obj array
-const createAllBooksArray = () => {
+// // Return a book obj array
+// const createAllBooksArray = () => {
 
-    let allBooks = [
+//     let allBooks = [
 
-        {title: "book 1", author: "author 1", image: "./img/1.jpg", alreadyRead : true},
-        {title: "book 2", author: "author 2", image: "./img/2.jpg", alreadyRead : false},
-        {title: "book 3", author: "author 3", image: "./img/3.jpg", alreadyRead : true},
-        {title: "book 1", author: "author 1", image: "./img/1.jpg", alreadyRead : true},
-        {title: "book 2", author: "author 2", image: "./img/2.jpg", alreadyRead : false},
-        {title: "book 3", author: "author 3", image: "./img/3.jpg", alreadyRead : false},
-        {title: "book 1", author: "author 1", image: "./img/1.jpg", alreadyRead : false},
-        {title: "book 2", author: "author 2", image: "./img/2.jpg", alreadyRead : false},
-        {title: "book 3", author: "author 3", image: "./img/3.jpg", alreadyRead : true}
+//         {title: "book 1", author: "author 1", image: "./img/1.jpg", alreadyRead : true},
+//         {title: "book 2", author: "author 2", image: "./img/2.jpg", alreadyRead : false},
+//         {title: "book 3", author: "author 3", image: "./img/3.jpg", alreadyRead : true},
+//         {title: "book 1", author: "author 1", image: "./img/1.jpg", alreadyRead : true},
+//         {title: "book 2", author: "author 2", image: "./img/2.jpg", alreadyRead : false},
+//         {title: "book 3", author: "author 3", image: "./img/3.jpg", alreadyRead : false},
+//         {title: "book 1", author: "author 1", image: "./img/1.jpg", alreadyRead : false},
+//         {title: "book 2", author: "author 2", image: "./img/2.jpg", alreadyRead : false},
+//         {title: "book 3", author: "author 3", image: "./img/3.jpg", alreadyRead : true}
         
-        ];
+//         ];
 
-        return allBooks;
-}
+//         return allBooks;
+// }
 
-// Create all book information table and return the table rag
-const createAllBooksTable = (allBooksArr) => {
+// // Create all book information table and return the table rag
+// const createAllBooksTable = (allBooksArr) => {
 
-    let tableTag = createSingleElement('table');
+//     let tableTag = createSingleElement('table');
 
-    createAllTrWithHeaders(allBooksArr, tableTag);
+//     createAllTrWithHeaders(allBooksArr, tableTag);
 
-    createAllTrWithData(allBooksArr, tableTag);
+//     createAllTrWithData(allBooksArr, tableTag);
 
-    return tableTag;
-}
-
-
-// Create all tr with headers inside and append to table tag 
-const createAllTrWithHeaders = (allBooksArr, tableTag) => {
-
-    let trTag = createSingleElement('tr');
-
-    // SCAN EACH *OBJECT* SO 'FOR IN' IS GOOD
-    for(let bookCategory in allBooksArr[0]){ // Create td's to each tr
-
-        let newThTag = createSingleElement('th', bookCategory);
-
-        // Append the new th to the tr tag
-        trTag.appendChild(newThTag);
-    }
-
-    // Append the new tr to the table tag
-    tableTag.appendChild(trTag);
-}
+//     return tableTag;
+// }
 
 
-// Return td tag with the ralevent data inside
-const getNewTdTagAcorrdingToDataOrImageInside = (book, bookCategory) =>{
+// // Create all tr with headers inside and append to table tag 
+// const createAllTrWithHeaders = (allBooksArr, tableTag) => {
 
-    let newTdTag;
-    let isRead = false;
+//     let trTag = createSingleElement('tr');
 
-    if(bookCategory === "image"){
+//     // SCAN EACH *OBJECT* SO 'FOR IN' IS GOOD
+//     for(let bookCategory in allBooksArr[0]){ // Create td's to each tr
 
-        newTdTag = createSingleElement('td');
-        newImageTag = createSingleElement('img');
+//         let newThTag = createSingleElement('th', bookCategory);
 
-        // Set attributes to image tag
-        newImageTag.setAttribute('src', book[bookCategory]);
-        newImageTag.classList.add('image')
+//         // Append the new th to the tr tag
+//         trTag.appendChild(newThTag);
+//     }
+
+//     // Append the new tr to the table tag
+//     tableTag.appendChild(trTag);
+// }
 
 
-        // Append the image tag to the td tag
-        newTdTag.appendChild(newImageTag);
+// // Return td tag with the ralevent data inside
+// const getNewTdTagAcorrdingToDataOrImageInside = (book, bookCategory) =>{
 
-    }
-    else{
+//     let newTdTag;
+//     let isRead = false;
 
-        newTdTag = createSingleElement('td', book[bookCategory]); 
+//     if(bookCategory === "image"){
 
-        if(bookCategory === "alreadyRead"){
+//         newTdTag = createSingleElement('td');
+//         newImageTag = createSingleElement('img');
 
-            isRead = book[bookCategory];
+//         // Set attributes to image tag
+//         newImageTag.setAttribute('src', book[bookCategory]);
+//         newImageTag.classList.add('image')
+
+
+//         // Append the image tag to the td tag
+//         newTdTag.appendChild(newImageTag);
+
+//     }
+//     else{
+
+//         newTdTag = createSingleElement('td', book[bookCategory]); 
+
+//         if(bookCategory === "alreadyRead"){
+
+//             isRead = book[bookCategory];
+//         }
+
+//     }
+
+//     return [newTdTag, isRead];
+// }
+
+
+// // Create all tr with data inside and append to table tag 
+// const createAllTrWithData = (allBooksArr, tableTag) => {
+
+//     // SCAN OBJECT *ARRAY* SO 'FOREACH' IS GOOD
+//     allBooksArr.forEach(book => { // Create tr tags according to the numbers of obj = books in array
+
+//         let isAlreadyRead;
+//         let trTag = createSingleElement('tr');
+
+//         // SCAN EACH *OBJECT* SO 'FOR IN' IS GOOD
+//         for(let bookCategory in book){ // Create td's to each tr
+
+//             let newTdTagAndIsRead = getNewTdTagAcorrdingToDataOrImageInside(book, bookCategory);
+
+//             let newTdTag = newTdTagAndIsRead[0];
+//             isAlreadyRead = newTdTagAndIsRead[1];
+
+//             // Append the new td to the tr tag
+//             trTag.appendChild(newTdTag);
+//         }
+
+//         // Set to red the table row if already read
+//         if(isAlreadyRead){
+
+//             trTag.setAttribute('class','red');           
+//         }
+
+//         // Append the new tr to the table tag
+//         tableTag.appendChild(trTag);
+//     });
+// }
+
+
+// // Return new element with txt inside (in case we have)
+// const createSingleElement = (tagNameToCreate, txtToTag = '') => { // Default parameter to txtToTag
+
+//     let newTag = document.createElement(tagNameToCreate);
+
+//     // Add txt to tag in case we have
+//     if(txtToTag != ''){
+
+//         let txtNode = document.createTextNode(txtToTag);
+//         newTag.appendChild(txtNode);
+//     }
+
+//     return newTag;
+// }
+
+// // Append the table tag to the div tag
+// const appendAllBooksTableToHtmlDiv = (allBooksTable) => {
+
+//     // Get the div we have to append to
+//     let whereAppendTheTable = document.getElementsByClassName('listBooks')[0];
+
+//     // Append the table tag to the div tag
+//     whereAppendTheTable.appendChild(allBooksTable);
+// }
+
+// // Start function
+// const startOfExe = () => {
+
+//     let allBooksArr = createAllBooksArray();
+
+//     let allBooksTable = createAllBooksTable(allBooksArr);
+
+//     appendAllBooksTableToHtmlDiv(allBooksTable);
+// }
+
+
+// /***************************************************************************/
+
+// // Calling to function 
+// startOfExe();
+
+
+
+
+
+
+
+//*************************************************************************
+
+// Exe no 2
+
+let table = document.body.firstElementChild;
+
+console.log(table)
+
+let allTrs = table.children[0].getElementsByTagName('tr');
+
+
+for(let i=0; i<allTrs.length; i++){
+
+    let allRdsPerRow = allTrs[i].getElementsByTagName('td');
+
+    for(let j=0; j<allRdsPerRow.length; j++){
+
+        if(i === j){
+
+            allRdsPerRow[j].setAttribute('class','red'); 
         }
 
-    }
+        if(i === (allRdsPerRow.length-1)-j ){
 
-    return [newTdTag, isRead];
-}
-
-
-// Create all tr with data inside and append to table tag 
-const createAllTrWithData = (allBooksArr, tableTag) => {
-
-    // SCAN OBJECT *ARRAY* SO 'FOREACH' IS GOOD
-    allBooksArr.forEach(book => { // Create tr tags according to the numbers of obj = books in array
-
-        let isAlreadyRead;
-        let trTag = createSingleElement('tr');
-
-        // SCAN EACH *OBJECT* SO 'FOR IN' IS GOOD
-        for(let bookCategory in book){ // Create td's to each tr
-
-            let newTdTagAndIsRead = getNewTdTagAcorrdingToDataOrImageInside(book, bookCategory);
-
-            let newTdTag = newTdTagAndIsRead[0];
-            isAlreadyRead = newTdTagAndIsRead[1];
-
-            // Append the new td to the tr tag
-            trTag.appendChild(newTdTag);
+            allRdsPerRow[j].setAttribute('class','coral'); 
         }
-
-        // Set to red the table row if already read
-        if(isAlreadyRead){
-
-            trTag.setAttribute('class','red');           
-        }
-
-        // Append the new tr to the table tag
-        tableTag.appendChild(trTag);
-    });
-}
-
-
-// Return new element with txt inside (in case we have)
-const createSingleElement = (tagNameToCreate, txtToTag = '') => { // Default parameter to txtToTag
-
-    let newTag = document.createElement(tagNameToCreate);
-
-    // Add txt to tag in case we have
-    if(txtToTag != ''){
-
-        let txtNode = document.createTextNode(txtToTag);
-        newTag.appendChild(txtNode);
     }
-
-    return newTag;
 }
-
-// Append the table tag to the div tag
-const appendAllBooksTableToHtmlDiv = (allBooksTable) => {
-
-    // Get the div we have to append to
-    let whereAppendTheTable = document.getElementsByClassName('listBooks')[0];
-
-    // Append the table tag to the div tag
-    whereAppendTheTable.appendChild(allBooksTable);
-}
-
-// Start function
-const startOfExe = () => {
-
-    let allBooksArr = createAllBooksArray();
-
-    let allBooksTable = createAllBooksTable(allBooksArr);
-
-    appendAllBooksTableToHtmlDiv(allBooksTable);
-}
-
-
-/***************************************************************************/
-
-// Calling to function 
-startOfExe();
-
-
-
 
 
 
