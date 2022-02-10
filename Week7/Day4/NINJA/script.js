d62d0f0dd7fbab25272866550e117494848ab6bd
HalifAvi <96798674+HalifAvi@users.noreply.github.com>
2022-02-10 13:18:09 +0200

    ninja

---
+++ Week7/Day4/NINJA/script.js
@@ -0,0 +1,17 @@
+class Bird {
+    constructor() {
+      console.log("I'm a bird. 🦢");
+    }
+  }
+  
+  class Flamingo extends Bird {
+    constructor() {
+      console.log("I'm pink. 🌸");
+      super();
+    }
+  }
+  
+  const pet = new Flamingo();
+
+  // "I'm pink. 🌸"
+  // "I'm a bird. 🦢"
