365cc3f1b79ab104e4192326cea199d94e42bd98
HalifAvi <96798674+HalifAvi@users.noreply.github.com>
2022-02-20 14:48:39 +0200

    weather

--- Week8/Day5/Weather_App/script.js
+++ Week8/Day5/Weather_App/script.js
@@ -1,66 +1,156 @@
 
+let allPrevCityTemp = [];
+
+let changeBackgroundColor = e => {
+
+    e.target.classList.toggle("redBackground");
+}
+
+let removeCard = e => {
+
+    let displayDiv = e.target.parentElement.parentElement;
+    let cardDiv = e.target.parentElement;
+    displayDiv.removeChild(cardDiv);
+
+    let idxToRemove = allPrevCityTemp.findIndex( element => element.city === e.target.parentElement.id);
+    allPrevCityTemp.splice(idxToRemove, 1); // 2nd parameter means remove one item only
+}
+
+let displayUserLocation = () => {
+
+	jQuery.get("http://ipinfo.io",function(e){
+        makeRequest(e.city, true); 
+	},"jsonp")
+}
+
+
+let changeTempUnit = e => {
+
+    if(allPrevCityTemp.length > 0){
+
+        if(e.target.checked) {
+            
+            allPrevCityTemp.forEach(element => {
+
+                let temperatureElement  = document.querySelector(`div#${(element.city).replace(/\s/g, '')}`).children.tempNum;
+                temperatureElement.innerHTML = (((element.temp)*9/5) + 32).toFixed(0) + "&#x2109;";
+            }); 
+
+        } else {
+
+            allPrevCityTemp.forEach(element => {
+
+                let temperatureElement  = document.querySelector(`div#${(element.city).replace(/\s/g, '')}`).children.tempNum;
+                temperatureElement.innerHTML = element.temp +  "&#x2103;";
+            });  
+
+        }
+    }
+}
 
 class TemperatureCard{
 
-    constructor( { sys : {country}, main : {temp}, weather : [{description}], name } ){ 
+    constructor( { sys : {country}, main : {temp}, weather : [{description, icon}], name } ){ 
      
         this.country = country;
         this.city = name;
-        this.temp = temp;
+        this.temp = parseFloat(temp).toFixed(0);
         this.description = description;
+        this.iconCode = icon;
     }
 
-    createHtml(){
+    
+
+    createHtml(userLocation){
+
+        let whereToAppend;
+
+        userLocation ?  whereToAppend = document.querySelector('#col1Title') : whereToAppend = document.querySelector('#col1CitiesDisplay');
 
         let cardDiv = document.createElement('div');
         cardDiv.classList.add('card');
         cardDiv.style.width = "18rem";
         cardDiv.style.display = "inline-block";
-        cardDiv.style.margin = "20px";
-        cardDiv.style.textAlign = "center"; 
-        cardDiv.style.background = "#CAA7DE";
+        cardDiv.style.marginTop = "100px";
+        cardDiv.style.marginRight = "20px";
+        userLocation ? cardDiv.style.background = "red" : cardDiv.style.background = "#7887AB";
         cardDiv.style.padding = "20px"; 
+        cardDiv.style.border = "2px solid white"; 
+        cardDiv.style.position = "relative";
+        cardDiv.setAttribute('id',(this.city).replace(/\s/g, ''));
 
         let cityName = document.createElement('h2');
         cityName.textContent = this.city;
+        cityName.style.display = "inline-block";
+        cityName.style.verticalAlign = "text-top";
         let countryName = document.createElement('h6');
         countryName.textContent = this.country;
+        countryName.style.borderRadius = "40%";
+        countryName.style.width = "30px";
+        countryName.style.color = "white";
+        countryName.style.display = "inline-block";
+        countryName.style.background = "orange";
+        countryName.style.margin = "10px";
         let temperature = document.createElement('h1');
         temperature.textContent = this.temp;
-        let description = document.createElement('h1');
+        temperature.innerHTML = this.temp + "&#x2103;";
+        temperature.style.fontSize = "80px";
+        temperature.setAttribute("name","tempNum");
+        let removeBtt = document.createElement('h5');
+        removeBtt.textContent = "X";
+        removeBtt.style.position = "absolute";
+        removeBtt.style.top = "10px";
+        removeBtt.style.right = "10px";
+        removeBtt.style.color = "white";
+
+        let description = document.createElement('h5');
         description.textContent = this.description;
+        let icon = document.createElement('img');
+        icon.src = `http://openweathermap.org/img/wn/${this.iconCode}@2x.png`;
+        icon.alt = this.iconCode;
+
+        if(userLocation){
+
+            let userLocationTitle = document.createElement('h3');
+            userLocationTitle.textContent = "YOUR LOCATION : ";
+            userLocationTitle.style.color = "white";
+            cardDiv.appendChild(userLocationTitle);
+        }
 
         cardDiv.appendChild(cityName);
         cardDiv.appendChild(countryName);
         cardDiv.appendChild(temperature);
+        cardDiv.appendChild(icon);
         cardDiv.appendChild(description); 
 
-        let whereToAppend = document.querySelector('#col1CitiesDisplay');
+        if(!userLocation){
+
+            removeBtt.addEventListener('mouseover', changeBackgroundColor);
+            removeBtt.addEventListener('mouseout', changeBackgroundColor);
+            removeBtt.addEventListener('click', removeCard);
+            cardDiv.appendChild(removeBtt); 
+        }
+     
         whereToAppend.appendChild(cardDiv);
     }
 }
 
-
-let makeRequest = e => {
-
-    e.preventDefault();
-
-    let inputCityValue =  (e.target).cityInput.value;
+let makeRequest = (cityName, userLocation) => {
 
     let xhr = new XMLHttpRequest();
 
     let rParams = {
 
-        q : inputCityValue,
+        q : cityName,
         appid : `6bc236fa8bd5e7e03f83fd8cea3eac74`,
         units : `metric`,
-   
+
         generatorParams (){
 
             return Object.entries(this) // Create [value,key] array from this object
-                   .filter( ([key, value]) => typeof value !== "function") // Remove the function
-                   .map( ([key, value]) => `${key}=${value}`) // Create an array of strings 
-                   .join("&") // Convert array to one big string and put '&' between the elements 
+                .filter( ([key, value]) => typeof value !== "function") // Remove the function
+                .map( ([key, value]) => `${key}=${value}`) // Create an array of strings 
+                .join("&") // Convert array to one big string and put '&' between the elements 
         }
     }
 
@@ -70,21 +160,18 @@ let makeRequest = e => {
 
     xhr.onload = function() {
 
-        if(xhr.status != 200){
-    
-    
+        if(xhr.status !== 200){
+
+            alert("Ooops! Your location could not be found.")
         }
         else{
-    
-            console.log(xhr.response);
     
             let data = xhr.response;
     
             let currentTempCard = new TemperatureCard(data);
-    
-            console.log(currentTempCard);
-    
-            currentTempCard.createHtml();
+
+            allPrevCityTemp.push(currentTempCard);
+            currentTempCard.createHtml(userLocation);
         }
     }
 
@@ -92,8 +179,29 @@ let makeRequest = e => {
 }
 
 
+let displayTempCard = e => {
+
+    e.preventDefault();
+
+    let inputCityValue =  (e.target).cityInput.value;
+
+    if(inputCityValue !== ""){
+
+        if( ! (allPrevCityTemp.find( (element => element.city.toLowerCase() ===  inputCityValue.toLowerCase() )))){
+
+            makeRequest(inputCityValue, false);
+          
+        }
+    
+        (document.querySelector('#searchingInput')).value = "";
+    }
+}
 
 
 
+(document.querySelector('form')).addEventListener("submit", displayTempCard);
+(document.querySelector('#toggleswitch')).addEventListener("change" ,changeTempUnit);
+displayUserLocation();
 
-(document.querySelector('form')).addEventListener("submit", makeRequest);
+
+
