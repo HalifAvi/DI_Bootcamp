38d000f944a4b23b3f086a5b95c724a1accc57c9
HalifAvi <96798674+HalifAvi@users.noreply.github.com>
2022-02-21 17:05:54 +0200

    date

--- Week8/Day5/Dates/script.js
+++ Week8/Day5/Dates/script.js
@@ -1,32 +1,35 @@
 
+let addOneIfMonth = (value, key) => key === "getMonth" ? (value + 1).toString() : value.toString();
+
 let formatDate = (dateObj) => {
 
-    let year = dateObj.getFullYear();
-    let month; 
-    let day;
-    let hours;
-    let minutes;
-    let seconds;
+    let obj = {
+               getFullYear: "",
+               getMonth: "",
+               getDate: "",
+               getHours: "",
+               getMinutes: "", 
+               getSeconds: "",
+    };
 
-    // Be aware that the Date's month field is zero based (0 is January and 11 is December).
-    (dateObj.getMonth()).toString().length == 2 ?  month = (dateObj.getMonth()+1).toString() : month = "0" + (dateObj.getMonth()+1);
 
-    (dateObj.getDate()).toString().length == 2 ?  day = (dateObj. getDate()).toString() : day = "0" + (dateObj.getDate());
+    for (let key in obj){
 
-    (dateObj.getHours()).toString().length == 2 ?  hours = (dateObj. getHours()).toString() : hours = "0" + (dateObj.getHours());
-    
-    (dateObj.getMinutes()).toString().length == 2 ?  minutes = (dateObj.getMinutes()).toString() : minutes = "0" + (dateObj.getMinutes());
+        // Way to use string as a function name. The function calling : obj[stringAsFunName]()
+        let value = dateObj[key]();
+        ((dateObj[key]()).toString()).length < 2  ?  obj[key] = "0" + addOneIfMonth(value, key) : obj[key] = addOneIfMonth(value, key);
+    }
 
-    (dateObj.getSeconds()).toString().length == 2 ?  seconds = (dateObj.getSeconds()).toString() : seconds = "0" + (dateObj.getSeconds());
-
-    return year + month + day + hours + minutes + seconds;
+    return Object.values(obj).join('');
 }
 
 
 
+    
+
 
 console.log(formatDate(new Date(2020, 6, 4, 8, 0, 0))) //➞ "20200704080000"
 
-console.log(formatDate(new Date(2019, 11, 31, 23, 59, 59))) //➞ "20191231235959"
+console.log(formatDate(new Date(2019, 11, 31, 23, 59, 59))) // "20191231235959"
 
-console.log(formatDate(new Date(2020, 6, 4))) //➞ "20200704000000"
+console.log(formatDate(new Date(2020, 6, 4))) // "20200704000000"
