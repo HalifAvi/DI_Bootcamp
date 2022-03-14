bdd26934e4c49f08af14c03513639364cbcf625d
HalifAvi <96798674+HalifAvi@users.noreply.github.com>
2022-03-14 14:42:28 +0000

    daily

--- Week12/Day2/Modules/main.js
+++ Week12/Day2/Modules/main.js
@@ -6,10 +6,37 @@
 // In the script.js file declare a variable const b = 5; and use it to output the sum of the imported module + b
 
 
-const largeNumber = 356;
+// const largeNumber = 356;
+
+// module.exports = {
+
+//     a : largeNumber
+
+// }
+
+
+
+// Part III:
+// Create a file named main.js and create a function that returns the current date and time. Export the module.
+
+// Use the exported module in a script.js file.
+
+// Create a server with http, set the response header and respond with a message that outputs the current date and time from the exported module.
+
+// Your server should run on http://localhost:8080/
+
+const getCurrentDateTime = () => {
+
+    let current = new Date();
+    let cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
+    let cTime = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
+    let dateTime = cDate + ' ' + cTime;
+    return dateTime;
+}
+
 
 module.exports = {
 
-    a : largeNumber
-
+    getCurrentDateTime : getCurrentDateTime
 }
+
--- Week12/Day2/Modules/script.js
+++ Week12/Day2/Modules/script.js
@@ -1,4 +1,52 @@
-const b = 5;
-const a = require('./main.js');
+// Part I:
+// Create a file named main.js that contains a variable const largeNumber = 356; and export the module.
 
-console.log(a.a + b);
+// Use the exported module in a script.js file.
+
+// In the script.js file declare a variable const b = 5; and use it to output the sum of the imported module + b
+
+
+
+// const b = 5;
+// const a = require('./main.js');
+// const c = a.a + b;
+// console.log();
+
+
+
+// Part II:
+// In a script.js file create a server with http and console.log a message.
+
+// Set the response header, and respond by the result from Part I.
+
+// Also write in a head tag a message like ‘Hi there at the frontend’
+
+// Your server should run on http://localhost:3000/
+
+
+// const http = require('http');
+// const server = http.createServer((req,res) => {
+
+//     console.log('Im listening.......');
+//     res.end(`My Moudle is: ${c}, Hi there at the fronted!`);
+// }); 
+
+// server.listen(3000);
+
+
+// Part 3:
+
+const dateAndTime = require('./main.js');
+
+
+const http = require('http');
+const server = http.createServer((req,res) => {
+
+    console.log('Im listening.......');
+    res.end(`The date and time are currently : ${dateAndTime.getCurrentDateTime()}`);
+}); 
+
+server.listen(8080);
+
+
+
