3491a1df69f491104ecd9833b8c58ae438bce706
HalifAvi <96798674+HalifAvi@users.noreply.github.com>
2022-04-04 17:09:24 +0300

    f.s

---
+++ Week15/fullstackexc/nodejsserver/modules/db.js
@@ -0,0 +1,48 @@
+const knex = require('knex');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const db = knex({
+
+    client: 'pg',
+    connection:{
+
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        sslmode : false
+    }
+});
+
+
+/*
+*
+*This function get the all countries
+*/
+const getCountries = () => {
+
+    return  db('country')
+    .select('country_id', 'country');
+} 
+
+/*
+*
+*This function get the all cities of specific country according to the country id
+*/
+const getCities = countryId => {
+
+    return  db('city')
+    .select('city', 'country_id')
+    .where({country_id : countryId})
+} 
+
+
+
+module.exports = {
+
+    getCountries,
+    getCities
+}
---
+++ Week15/fullstackexc/nodejsserver/server.js
@@ -0,0 +1,49 @@
+
+const express = require('express');
+const DB = require('./modules/db.js');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+app.listen(process.env.PORT||8080, ()=> {
+
+    console.log(`listen to port ${process.env.PORT}`);
+})
+
+
+
+app.get('/countries', (req, res) => {
+
+    DB.getCountries() 
+    .then(countries => {
+
+        res.send(countries);
+    })
+    .catch(e=> res.json({message: e.message}));
+})
+
+
+app.get('/cities/:id', (req, res) => {
+
+    DB.getCities(req.params.id) 
+    .then(cities => {
+
+        res.send(cities);
+    })
+    .catch(e=> res.json({message: e.message}));
+})
+
+
+
+
+
+
+
+
+
+
+
+
