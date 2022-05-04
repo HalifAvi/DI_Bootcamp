6269bc8854fc9cbb376d70ad3b7ac0ef3f5dd425
HalifAvi <96798674+HalifAvi@users.noreply.github.com>
2022-05-04 12:20:57 +0300

    maps

--- Week18/ma-p-goo-gle/src/App.js
+++ Week18/ma-p-goo-gle/src/App.js
@@ -7,7 +7,7 @@ import WrappedMap from './components/WrappedMap';
 function App() {
   return (
     <div style={{width: "100vw", height: "100vh"}}>
-      <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
+      <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBj3SEQEo5iI3xgvzRWieF-b0JoEXuJGbY`}
                   loadingElement={<div style={{height: "100%"}}/>}
                   containerElement={<div style={{height: "100%"}}/>}
                   mapElement={<div style={{height: "100%"}}/>}
