d4eaad08014ca4baa84cc6eb79cf2e570f2ecb1f
HalifAvi <96798674+HalifAvi@users.noreply.github.com>
2022-05-09 15:00:34 +0300

    cycle

--- FinalProject/client-side/src/App.js
+++ FinalProject/client-side/src/App.js
@@ -9,42 +9,44 @@ function App() {
 
   useEffect( ()=> {
 
-    fetch(`https://api.spoonacular.com/recipes/findByNutrients?apiKey=${YOUR_API_KEY}&number=1&maxCalories=300`)
+    // MORE THAN 8 WITH THE CURRENT PIC SIZE WE CAN'T SEE IT
+    fetch(`https://api.spoonacular.com/recipes/findByNutrients?apiKey=${YOUR_API_KEY}&number=8&maxCalories=300`)
     .then(res=>res.json()) 
     .then(data=>{
       
-      console.log(data[0])
-
-      const {calories, title, image, id} = data[0];
-
-      // setRecepies(data)
-
-      fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${YOUR_API_KEY}&includeNutrition=true`)
-      .then(res=>res.json()) 
-      .then(data=>{
-        
         console.log(data)
 
+        // const {calories, title, image, id} = data[0];
 
+        // console.log(data)
+
+        setRecepies(data)
+
+        // fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${YOUR_API_KEY}&includeNutrition=true`)
+        // .then(res=>res.json()) 
+        // .then(data=>{
+          
+        //   console.log(data)
+
+
+        // })
+        // .catch(e=>console.log(e))
       })
       .catch(e=>console.log(e))
-
-  
-
-
-      })
-      .catch(e=>console.log(e))
-
-
-
   }, [])
 
 
 
   return (
-    <div>
 
-      
+    <div className='box'>
+      {
+        recepies.map((recepie, idx) => {
+
+          return <span key={idx} style={{"--i":`${idx+1}`}}><img src={recepie.image}/></span>
+
+        })
+      }
     </div>
   );
 }
