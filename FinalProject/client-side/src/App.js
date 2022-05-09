cb2fa6c328f8d7fbfadb8965b610b5cb70edcda8
HalifAvi <96798674+HalifAvi@users.noreply.github.com>
2022-05-09 12:42:19 +0300

    structure

--- FinalProject/client-side/src/App.js
+++ FinalProject/client-side/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+
+const YOUR_API_KEY = 'd58baefbd9bf4263946475c94f999e9d';
 
 function App() {
+
+  const [recepies, setRecepies] = useState([]);
+
+  useEffect( ()=> {
+
+    fetch(`https://api.spoonacular.com/recipes/findByNutrients?apiKey=${YOUR_API_KEY}&number=1&maxCalories=300`)
+    .then(res=>res.json()) 
+    .then(data=>{
+      
+      console.log(data[0])
+
+      const {calories, title, image, id} = data[0];
+
+      // setRecepies(data)
+
+      fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${YOUR_API_KEY}&includeNutrition=true`)
+      .then(res=>res.json()) 
+      .then(data=>{
+        
+        console.log(data)
+
+
+      })
+      .catch(e=>console.log(e))
+
+  
+
+
+      })
+      .catch(e=>console.log(e))
+
+
+
+  }, [])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      
     </div>
   );
 }
