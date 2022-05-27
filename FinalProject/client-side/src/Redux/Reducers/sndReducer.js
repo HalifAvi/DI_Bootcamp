5b5bb6139a5f5014a22cb27d60a98da603efcf8d
HalifAvi <96798674+HalifAvi@users.noreply.github.com>
2022-05-27 09:29:06 +0300

    redux

---
+++ FinalProject/client-side/src/Redux/Reducers/sndReducer.js
@@ -0,0 +1,18 @@
+
+const initState = {
+
+
+
+}
+
+export const sndReducer = (state=initState, action={}) => {
+
+    switch(action.type){
+
+        case 'AAA': 
+            return {...state}
+
+        default: 
+            return {...state}
+    }
+}
