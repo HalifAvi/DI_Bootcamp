2a22f53f8e7facec97cfd5cf73c726b1ab8b1b82
HalifAvi <96798674+HalifAvi@users.noreply.github.com>
2022-05-30 11:35:27 +0300

    prj

--- FinalProject/client-side/src/components/pages/LoginSigninForm.js
+++ FinalProject/client-side/src/components/pages/LoginSigninForm.js
@@ -16,13 +16,25 @@ const LoginSigninForm = ({pageToDisplay}) => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [age, setAge] = useState('');
+    const [height, setHeight] = useState('');
+    const [weight, setWeight] = useState('');
+    const [gender, setGender] = useState('');
+    const [sport, setSport] = useState('');
     const [message, setMessage] = useState('');
+
+
+    
 
     const navigate = useNavigate();
 
+    let signInDiv;
+
     useEffect(()=>{
 
-        setMessage(''); // Delete the message when mounting
+        // setMessage(''); // Delete the message when mounting
 
     }, [])
 
@@ -102,8 +114,33 @@ const LoginSigninForm = ({pageToDisplay}) => {
                 <TextField onChange={(e)=>setPassword(e.target.value)} sx={{m:1}} id='password' label='Enter the password' />
             </Box>
             <Button onClick={()=>handleAction(pageToDisplay)} variant="contained">{pageToDisplay}</Button>
+            {
+                
+                pageToDisplay === process.env.REACT_APP_SIGN_IN_NAME ? 
+
+                <Box component='form' sx={{m:1}} noValidate autoComplete="off">
+                    <TextField onChange={(e)=>setFirstName(e.target.value)} sx={{m:1}} id='firstName' label='Enter your first name' />
+                    <TextField onChange={(e)=>setLastName(e.target.value)} sx={{m:1}} id='lastName' label='Enter your last name' /> 
+                    <TextField onChange={(e)=>setAge(e.target.value)} sx={{m:1}} id='age' label='Enter your age' /> 
+                    <TextField onChange={(e)=>setHeight(e.target.value)} sx={{m:1}} id='height' label='Enter your height' /> 
+                    <TextField onChange={(e)=>setWeight(e.target.value)} sx={{m:1}} id='weight' label='Enter your weight' /> 
+
+                    <Button onClick={()=>setGender("male")} variant="contained">Male</Button>       
+                    <Button onClick={()=>setGender("female")} variant="contained">Female</Button>     
+
+                    <Button onClick={()=>setSport("")} variant="contained">Lowest</Button>       
+                    <Button onClick={()=>setSport("")} variant="contained">Low</Button>  
+                    <Button onClick={()=>setSport("")} variant="contained">Medium</Button>       
+                    <Button onClick={()=>setSport("")} variant="contained">High</Button>           
+                </Box>
+                
+                :
+
+                null
+            }
         </div>
     )
-}
+}     
+
 
 export default LoginSigninForm;
