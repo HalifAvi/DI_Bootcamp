aa74b0502ee29fdc37500514619dcee4324e2e93
HalifAvi <96798674+HalifAvi@users.noreply.github.com>
2022-06-08 23:33:13 +0300

    prj

--- FinalProject/client-side/src/components/pages/Main.js
+++ FinalProject/client-side/src/components/pages/Main.js
@@ -1,8 +1,13 @@
 import { useNavigate } from 'react-router-dom';
-import { useState, useEffect, createContext } from 'react';
+import { useState, createContext } from 'react';
 import { connect } from 'react-redux';
+import React from "react";
+import Video from "../BasicElements/Video";
+import AppLogo from "../BasicElements/AppLogo";
+import Title from "../BasicElements/Title";
 
-export const MainContext = createContext(null);
+import '../PagesStyle/Main.css';
+
 
 const Main = (props) => {
 
@@ -11,45 +16,72 @@ const Main = (props) => {
 
     return(
 
-
-
-        <>
-            <h1>MAIN</h1>
-            <button>Logout</button>
-            <button onClick={()=>{navigate("/protected")}}>Protected</button>
-
-            
-                {props.userId}<br/>
-                {props.email}<br/>
-                {props.gender}<br/>
-                {props.firstName}<br/>
-                {props.lastName}<br/>
-                {props.age}<br/>
-                {props.height}<br/>
-                {props.weight}<br/>
-                {props.activityLevel}<br/>
-                {props.fileName}<br/>
-                <img style={{width: "100px", height: "100px"}} src={`http://localhost:5000/uploads/${props.fileName}`}/>
-
-        </>
+        <div>  
+            <Video src={process.env.REACT_APP_BASE_VIDEO_MAIN_URL} id={"signin-back-video"} autoPlay muted loop />
+        
+            <div id={"main-left-side-div"}>
+        
+                <div id={"signinSignUpIntro-logo-div"}>
+                    <AppLogo id={"signinSignUpIntro-logo"} />
+                </div>
+        
+                <div id={"signinSignUpIntro-snd-title-div"}>
+                    <Title id={"signinSignUpIntro-snd-title"} typing={"typewriter"} 
+                            titleName={process.env.REACT_APP_SIGNUP_SND_TITLE} />
+                </div>    
+            </div>
+    </div>
     )
 }
+
+
+
+
+
 
 const mapStateToProps = (state) => {
 
     return{
 
-        userId : state.signInUpReducer.userId,
-        email : state.signInUpReducer.email,
-        gender : state.signInUpReducer.gender,
-        firstName : state.signInUpReducer.firstName,
-        lastName : state.signInUpReducer.lastName,
-        age : state.signInUpReducer.age,
-        height : state.signInUpReducer.height,
-        weight : state.signInUpReducer.weight,
-        activityLevel : state.signInUpReducer.activityLevel,
-        fileName : state.signInUpReducer.fileName
+        // userId : state.signInUpReducer.userId,
+        // email : state.signInUpReducer.email,
+        // gender : state.signInUpReducer.gender,
+        // firstName : state.signInUpReducer.firstName,
+        // lastName : state.signInUpReducer.lastName,
+        // age : state.signInUpReducer.age,
+        // height : state.signInUpReducer.height,
+        // weight : state.signInUpReducer.weight,
+        // activityLevel : state.signInUpReducer.activityLevel,
+        // fileName : state.signInUpReducer.fileName
     }
 }
 
 export default connect(mapStateToProps, null)(Main);
+
+
+
+    //     <>
+    //     <h1>MAIN</h1>
+    //     <button>Logout</button>
+    //     <button onClick={()=>{navigate("/protected")}}>Protected</button>
+
+        
+    //         {props.userId}<br/>
+    //         {props.email}<br/>
+    //         {props.gender}<br/>
+    //         {props.firstName}<br/>
+    //         {props.lastName}<br/>
+    //         {props.age}<br/>
+    //         {props.height}<br/>
+    //         {props.weight}<br/>
+    //         {props.activityLevel}<br/>
+    //         {props.fileName}<br/>
+    //         <img style={{width: "100px", height: "100px"}} src={`http://localhost:5000/uploads/${props.fileName}`}/>
+
+    // </>
+
+
+
+
+
+
