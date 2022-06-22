319af026978ed08a86ee8aff97a93d19aadade33
HalifAvi <96798674+HalifAvi@users.noreply.github.com>
2022-06-23 00:22:15 +0300

    prj

--- FinalProject/client-side/src/App.js
+++ FinalProject/client-side/src/App.js
@@ -9,12 +9,28 @@ import GeneralRecepies from './Components/Pages/GeneralRecepies';
 import TodayRecpies from './Components/Pages/TodayRecpies';
 import PreviousRecepies from './Components/Pages/PreviousRecepies';
 import Settings from './Components/Pages/Settings';
-
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { setAllUserParamsFromDb} from "./Redux/Actions/signInUpActions";
+import { setAmountOfCalories } from "./Redux/Actions/caloriesActions.js";
+import { setAllDefaultRecipesArray } from "./Redux/Actions/recipesActions.js";
+import { setTodayRecipesArray, setFavoritesRecpies }  from "./Redux/Actions/recipesActions.js";
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import { useNavigate } from "react-router-dom";
 
 
 
 
 function App() {
+
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+
+     //Will refresh page at 00:00:05 and 30sec, daily.
+    refreshAt(21,0,5, navigate);
+  })
 
   return (
     <div>
@@ -38,8 +54,48 @@ function App() {
   )
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
 
+  return {
+
+    setAllUserParamsFromDb : (allUserParamsObj) => dispatch( setAllUserParamsFromDb(allUserParamsObj) ),
+    setAmountOfCalories : (caloriesObj) => dispatch( setAmountOfCalories(caloriesObj) ),
+    setAllDefaultRecipesArray : () => dispatch( setAllDefaultRecipesArray() ),
+    setTodayRecipesArray : (allTodayRecipes) => dispatch( setTodayRecipesArray(allTodayRecipes) ),
+    setFavoritesRecpies : (favoritesRecpiesArr) => dispatch( setFavoritesRecpies(favoritesRecpiesArr) )
+  }
+}
+
+export default connect(null, mapDispatchToProps)(App);
+
+
+
+function refreshAt(hours, minutes, seconds, navigate) {
+
+
+  let now = new Date();
+  let then = new Date();
+
+
+      if(now.getUTCHours() > hours ||
+        (now.getUTCHours() == hours && now.getUTCMinutes() > minutes) ||
+        now.getUTCHours() == hours && now.getUTCMinutes() == minutes && now.getUTCSeconds() >= seconds) {
+        then.setUTCDate(now.getUTCDate() + 1);
+      }
+
+      then.setUTCHours(hours);
+      then.setUTCMinutes(minutes);
+      then.setUTCSeconds(seconds);
+      const timeout = (then.getTime() - now.getTime());
+      setTimeout(function() { 
+        
+        window.location.reload(true);
+
+        // Navigate outside the app cause it's the end of the day!!! 
+        navigate(process.env.REACT_APP_BASE_SIGN_IN_FORM_PATH);
+    
+      }, timeout);
+}
 
 
 
